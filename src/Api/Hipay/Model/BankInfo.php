9b8224049a4e0d1e385fbc2c2def6dd609b58ee9
Ivanis KOUAME <ivanis.kouame@smile.fr>
2016-01-12 10:21:44 +0100

    Mirakl api compatibility code

--- src/Api/Hipay/Model/BankInfo.php
+++ src/Api/Hipay/Model/BankInfo.php
@@ -92,7 +92,7 @@ class BankInfo extends SoapModelAbstract
             $miraklData['payment_info'] : $miraklData['billing_info'];
         $this->bankName = $paymentData['bank_name'];
         $this->bankAddress = $paymentData['bank_street'];
-        $this->bankZipCode = $paymentData['zip_code'];
+        $this->bankZipCode = isset($paymentData['zip_code']) ? $paymentData['zip_code'] : isset($paymentData['bank_zip']) ;
         $this->bankCity = $paymentData['bank_city'];
         $this->swift = $paymentData['bic'];
         $this->iban = $paymentData['iban'];
