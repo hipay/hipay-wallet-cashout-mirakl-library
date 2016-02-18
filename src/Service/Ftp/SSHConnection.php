3d705e27eed40dd440b91f217b2779dbe0b8aead
Ivanis KOUAME <ivanis.kouame@smile.fr>
2016-02-18 15:34:19 +0100

    Add ssh_ftp specific implementation

--- src/Service/Ftp/SSHConnection.php
+++ src/Service/Ftp/SSHConnection.php
@@ -3,6 +3,8 @@
 namespace HiPay\Wallet\Mirakl\Service\Ftp;
 
 use Touki\FTP\Connection\Connection;
+use Touki\FTP\ConnectionInterface;
+use Touki\FTP\Exception\ConnectionUnestablishedException;
 
 /**
  * Represent a ssh connection.
@@ -10,15 +12,73 @@ use Touki\FTP\Connection\Connection;
  * @author    Ivanis Kouamé <ivanis.kouame@smile.fr>
  * @copyright 2015 Smile
  */
-class SSHConnection extends Connection
+class SSHConnection implements ConnectionInterface
 {
+    /** @var  string */
+    protected $host;
+
+    /** @var  string */
+    protected $port;
+
+    /** @var  array */
+    protected $method;
+
+    /** @var  array */
+    protected $callback;
+
+    /** @var  boolean */
+    protected $connected;
+
+    protected $stream;
+
+    /**
+     * SSHConnection constructor.
+     * @param string $host
+     * @param string $port
+     * @param array $method
+     * @param array $callback
+     */
+    public function __construct($host, $port, array $method = null, array $callback = null)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->method = $method;
+        $this->callback = $callback;
+        $this->connected = false;
+        $this->stream = false;
+    }
+
+
     /**
      * Calls the connector.
      *
      * @return bool TRUE on success, FALSE on failure
      */
-    public function doConnect()
+    public function connect()
     {
-        return ssh2_sftp(ssh2_connect($this->getHost(), $this->getPort()));
+        $this->stream = ssh2_sftp(ssh2_connect($this->host, $this->port, $this->method, $this->callback));
+    }
+
+    /**
+     * Returns the connection stream
+     * @return resource FTP Connection stream
+     * @throws ConnectionUnestablishedException
+     */
+    public function getStream()
+    {
+        if (!$this->isConnected()) {
+            throw new ConnectionUnestablishedException("Cannot get stream context. Connection is not established");
+        }
+        return $this->stream;
+    }
+
+    /**
+     * Tells wether the connection is established
+     *
+     * @return boolean TRUE if connected, FALSE if not
+     */
+    public function isConnected()
+    {
+        return $this->connected;
     }
 }
