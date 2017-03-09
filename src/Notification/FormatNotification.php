4e1cf053ca0a9a11029e012e271ec824a006e940
Johan Protin <jprotin@hipay.com>
2017-03-09 10:57:50 +0100

    optimization email

--- src/Notification/FormatNotification.php
+++ src/Notification/FormatNotification.php
@@ -22,17 +22,20 @@ class FormatNotification {
             $markdown .=
                     HiPay::LINEMKD . HiPay::SEPARMKD . '***' .
                     HiPay::LINEMKD . HiPay::SEPARMKD .
-                    HiPay::LINEMKD . HiPay::SEPARMKD .'* Shop ID Mirakl: ' . $infos['shopId'] .
-                    HiPay::LINEMKD . HiPay::SEPARMKD .'* ID of the Wallet: ' . $infos['HipayId'] .
-                    HiPay::LINEMKD . HiPay::SEPARMKD .'* Email Shop: ' . $infos['Email'] .
-                    HiPay::LINEMKD . HiPay::SEPARMKD .'* Type Message: ' . $infos['Type'] .
+                    HiPay::LINEMKD . HiPay::SEPARMKD .'- Shop ID Mirakl: ' . $infos['shopId'] .
+                    HiPay::LINEMKD . HiPay::SEPARMKD .'- ID of the Wallet: ' . $infos['HipayId'] .
+                    HiPay::LINEMKD . HiPay::SEPARMKD .'- Email Shop: ' . $infos['Email'] .
+                    HiPay::LINEMKD . HiPay::SEPARMKD .'- Type Message: ' . $infos['Type'] .
+                    HiPay::LINEMKD . HiPay::SEPARMKD .
                     HiPay::LINEMKD . HiPay::SEPARMKD . '***';
         }
         if ($message) {
-            $markdown .= HiPay::LINEMKD.HiPay::SEPARMKD.'```
-                '.HiPay::LINEMKD.HiPay::SEPARMKD.'Message:'.$message.'   
-                '.HiPay::LINEMKD.HiPay::SEPARMKD.'```
-                '.HiPay::LINEMKD.HiPay::SEPARMKD.'***'.HiPay::SEPARMKD;
+            $markdown .= HiPay::LINEMKD.HiPay::SEPARMKD . '```'.
+                    HiPay::LINEMKD.HiPay::SEPARMKD.'Message:'.$message.
+                    HiPay::LINEMKD.HiPay::SEPARMKD.
+                    '```'.
+                    HiPay::LINEMKD . HiPay::SEPARMKD .
+                    HiPay::LINEMKD.HiPay::SEPARMKD.'***';
         }
         return $markdown;
     }
