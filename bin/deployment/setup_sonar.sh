47e328c0f5dbc14870935965aeeacb9d452e0fbb
Etienne Landais <elandais@hipay.com>
2017-11-22 15:10:19 +0100

    Switch to circle ci 2

--- bin/deployment/setup_sonar.sh
+++ bin/deployment/setup_sonar.sh
@@ -1,5 +1,6 @@
 sudo apt-get install unzip
 sudo wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.0.3.778-linux.zip
-sudo unzip sonar-scanner-cli-3.0.3.778-linux.zip
-sudo chmod -R a+x sonar-scanner-3.0.3.778-linux/
-sudo ln -s --force ~/mirakl/sonar-scanner-3.0.3.778-linux/bin/sonar-scanner /usr/local/bin/sonar-scanner
+sudo mv sonar-scanner-cli-3.0.3.778-linux.zip ~/
+sudo unzip ~/sonar-scanner-cli-3.0.3.778-linux.zip
+sudo chmod -R a+x ~/sonar-scanner-3.0.3.778-linux/
+sudo ln -s --force ~/sonar-scanner-3.0.3.778-linux/bin/sonar-scanner /usr/local/bin/sonar-scanner
