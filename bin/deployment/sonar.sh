92efc86cf8e3e110a91e12dd9f388638bde2840f
Etienne Landais <elandais@hipay.com>
2017-11-22 14:15:00 +0100

    Switch to circle ci 2

--- bin/deployment/sonar.sh
+++ bin/deployment/sonar.sh
@@ -1,12 +1,13 @@
 if [ -z "$CIRCLE_PULL_REQUEST" ];then
     echo "Not a pull request"
 elif [ $CI_PULL_REQUESTS != "" ];then
-    PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f 7)
+    PR_NUMBER=$(echo $CIRCLE_PULL_REQUEST | cut -d'/' -f 7)
     sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.login=$SONAR_LOGIN -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PR_NUMBER -Dsonar.github.oauth=$GITHUB_TOKEN
 fi
+
 if [ -z $CIRCLE_BRANCH ];then
-    if [ $CIRCLE_BRANCH = "develop" ];then
-        sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.login=$SONAR_LOGIN -Dsonar.sources=. -Dsonar.php.tests.reportPath=$CIRCLE_ARTIFACTS/logs.xml  -Dsonar.php.coverage.reportPaths=$CIRCLE_ARTIFACTS/coverage.xml
-    fi
+    echo "Not a branch merge"
+elif [ $CIRCLE_BRANCH = "develop" ];then
+    sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.login=$SONAR_LOGIN -Dsonar.sources=. -Dsonar.php.tests.reportPath=$CIRCLE_ARTIFACTS/logs.xml  -Dsonar.php.coverage.reportPaths=$CIRCLE_ARTIFACTS/coverage.xml
 fi
 
